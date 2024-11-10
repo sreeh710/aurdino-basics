4f95a7026cefd73a08d5082da8b2f9823281cbe7
sreeh710 <sreeh710@gmail.com>
2024-11-09 20:33:07 -0500

    Adding Interrupt code

--- code/interrupt/interrupt.ino
+++ code/interrupt/interrupt.ino
@@ -1,1 +1,37 @@
+// C++ code
+//
 
+#define LED 12
+#define BUTTON 2
+byte LEDState = HIGH;
+volatile boolean release;
+void toggle() {
+  if(LEDState == LOW){
+  LEDState = HIGH;
+  } else {
+      LEDState = LOW;
+  }
+  digitalWrite(LED,LEDState);
+}
+void buttonReleaseInterrupt()
+{
+  release = true;
+}
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  attachInterrupt(digitalPinToInterrupt(BUTTON),
+                  buttonReleaseInterrupt,
+                  FALLING);
+
+}
+
+void loop()
+{
+  if(release) {
+    toggle();
+    release = false;
+  }
+}
